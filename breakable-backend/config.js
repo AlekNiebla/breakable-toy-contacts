04b8c6b921ef15f9d231bf81890820621eb56c94
AlekNiebla <aniebla@nearsoft.com>
2019-03-24 11:54:20 -0700

    front end skeleton WIP, no backend conenction yet

--- breakable-backend/config.js
+++ breakable-backend/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3001,
-    db : process.env.MONGODB||`mongodb+srv://user1:Welcome1..@breakabletoycontacts-fsa5m.mongodb.net/test?retryWrites=true`
+    db : process.env.MONGODB||`mongodb://localhost:27017/contacts`
 
 }
